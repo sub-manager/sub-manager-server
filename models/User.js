b97dca8d22c4c63cf85cdb0decb4085a35331679
Mosab albishi <90303281+musabalbishi@users.noreply.github.com>
2023-06-16 16:21:14 +0300

    created user  model

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  phone: {
+    type: String,
+    require: true,
+    min: [9, "phone number must have 9 numbers"],
+  },
+  password: {
+    type: String,
+    require: true,
+    min: [5, "password must be atleast 3 charachters"],
+  },
+});
+
+const User = mongoose.model("User", studentSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -7,6 +7,17 @@ const server = express();
 server.use(express.json());
 //
 
+mongoose
+  .connect(
+    "mongodb+srv://musab:8RNngQeVDJNhsh8M@sub.ya0nbol.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("database connected successfully");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
 server.get("/", (req, res) => {
   res.status(200).json({ success: "server is running" });
 });
